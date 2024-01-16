7d5809bc93227877d0dca7b7dac6e824baabd334
DernakovAV <dernakovs@mail.ru>
2024-01-16 20:17:08 +0300

    new file

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def user_input (str_1):
     str_1 = int(input())
     return str_1.upper()
+
+def user_input_1 (str_2):
+    """"Функция для озаглавливания первой буквы в тексте"""
+    str_2 = int(input())
+    return str_2.title()
